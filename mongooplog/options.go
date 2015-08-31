8946538354c432a4bb1ed979d59a1002fe853011
hustchensi <chensi_04@126.com>
2015-08-31 12:21:27 +0800

    add timestamp supported

--- mongooplog/options.go
+++ mongooplog/options.go
@@ -15,6 +15,7 @@ type SourceOptions struct {
 	From    string              `long:"from" description:"specify the host for mongooplog to retrive operations from"`
 	OplogNS string              `long:"oplogns" description:"specify the namespace in the --from host where the oplog lives (default 'local.oplog.rs') " default:"local.oplog.rs" default-mask:"-"`
 	Seconds bson.MongoTimestamp `long:"seconds" short:"s" description:"specify a number of seconds for mongooplog to pull from the remote host" default:"86400"  default-mask:"-"`
+	StartTs int64 		    `long:"startTs" short:"ts" description:"specify a timestamp for mongooplog to pull from the remote host" default:"0"  default-mask:"-"`
 }
 
 // Name returns a human-readable group name for source options.
