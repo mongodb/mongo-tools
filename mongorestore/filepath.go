0223f48b26690ff48cdc72c5144115196839aa71
Xinjiang Shao <shaoxinjiang@gmail.com>
2015-11-25 15:39:06 -0600

    TOOLS-976: Added support to exclude collections and exclude collection with certain prefix for mongorestore.

--- mongorestore/filepath.go
+++ mongorestore/filepath.go
@@ -272,6 +272,13 @@ func (restore *MongoRestore) CreateIntentsForDB(db string, filterCollection stri
 							"has .metadata.json files", db)
 					skip = true
 				}
+
+				// TOOLS-976: skip restoring the collections should be excluded 
+				if filterCollection == "" && restore.shouldSkipCollection(collection) {
+					log.Logf(log.DebugLow, "skipping restoring %v.%v, it is excluded", db, collection)
+					skip = true
+				}
+
 				if filterCollection != "" && filterCollection != collection {
 					skip = true
 				}
@@ -309,12 +316,19 @@ func (restore *MongoRestore) CreateIntentsForDB(db string, filterCollection stri
 				log.Logf(log.Info, "found collection %v bson to restore", intent.Namespace())
 				restore.manager.Put(intent)
 			case MetadataFileType:
+				// TOOLS-976: skip restoring the collections should be excluded 
+				if filterCollection == "" && restore.shouldSkipCollection(collection) {
+					log.Logf(log.DebugLow, "skipping restoring %v.%v metadata, it is excluded", db, collection)
+					continue	
+				}
+
 				usesMetadataFiles = true
 				intent := &intents.Intent{
 					DB:           db,
 					C:            collection,
 					MetadataPath: entry.Path(),
 				}
+				
 				if restore.InputOptions.Archive != "" {
 					if restore.InputOptions.Archive == "-" {
 						intent.Location = "archive on stdin"
@@ -411,6 +425,24 @@ func (restore *MongoRestore) CreateIntentForCollection(db string, collection str
 	restore.manager.Put(intent)
 
 	return nil
+}
+
+func (restore *MongoRestore) shouldSkipCollection(colName string) bool {
+	if restore.OutputOptions != nil && len(restore.OutputOptions.ExcludedCollections) > 0 {
+		for _, excludedCollection := range restore.OutputOptions.ExcludedCollections {
+			if colName == excludedCollection {
+				return true
+			}
+		}
+	}
+	if restore.OutputOptions != nil && len(restore.OutputOptions.ExcludedCollectionPrefixes) > 0 {
+		for _, excludedCollectionPrefix := range restore.OutputOptions.ExcludedCollectionPrefixes {
+			if strings.HasPrefix(colName, excludedCollectionPrefix) {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 // helper for searching a list of FileInfo for metadata files
