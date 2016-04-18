df3bcd412ad37a823e7b74948f1ef753055cbe45
Mike O'Brien <mpobrien005@gmail.com>
2016-04-18 11:06:22 -0400

    TOOLS-1141 add function to convert bson->json while deep copying argument

--- common/bsonutil/converter.go
+++ common/bsonutil/converter.go
@@ -128,6 +128,18 @@ func convertKeys(v bson.M) (bson.M, error) {
 	return v, nil
 }
 
+func getConvertedKeys(v bson.M) (bson.M, error) {
+	out := bson.M{}
+	for key, value := range v {
+		jsonValue, err := GetBSONValueAsJSON(value)
+		if err != nil {
+			return nil, err
+		}
+		out[key] = jsonValue
+	}
+	return out, nil
+}
+
 // ConvertBSONValueToJSON walks through a document or an array and
 // converts any BSON value to its corresponding extended JSON type.
 // It returns the converted JSON document and any error encountered.
@@ -222,6 +234,126 @@ func ConvertBSONValueToJSON(x interface{}) (interface{}, error) {
 		var err error
 		if v.Scope != nil {
 			scope, err = ConvertBSONValueToJSON(v.Scope)
+			if err != nil {
+				return nil, err
+			}
+		}
+		return json.JavaScript{v.Code, scope}, nil
+
+	default:
+		switch x {
+		case bson.MinKey: // MinKey
+			return json.MinKey{}, nil
+
+		case bson.MaxKey: // MaxKey
+			return json.MaxKey{}, nil
+
+		case bson.Undefined: // undefined
+			return json.Undefined{}, nil
+		}
+	}
+
+	return nil, fmt.Errorf("conversion of BSON type '%v' not supported %v", reflect.TypeOf(x), x)
+}
+
+// GetBSONValueAsJSON is equivalent to ConvertBSONValueToJSON, but does not mutate its argument.
+func GetBSONValueAsJSON(x interface{}) (interface{}, error) {
+	switch v := x.(type) {
+	case nil:
+		return nil, nil
+	case bool:
+		return v, nil
+
+	case *bson.M: // document
+		doc, err := getConvertedKeys(*v)
+		if err != nil {
+			return nil, err
+		}
+		return doc, err
+	case bson.M: // document
+		return getConvertedKeys(v)
+	case map[string]interface{}:
+		return getConvertedKeys(v)
+	case bson.D:
+		out := bson.D{}
+		for _, value := range v {
+			jsonValue, err := GetBSONValueAsJSON(value.Value)
+			if err != nil {
+				return nil, err
+			}
+			out = append(out, bson.DocElem{value.Name, jsonValue})
+		}
+		return MarshalD(out), nil
+	case MarshalD:
+		out, err := GetBSONValueAsJSON(bson.D(v))
+		if err != nil {
+			return nil, err
+		}
+		return MarshalD(out.(bson.D)), nil
+	case []interface{}: // array
+		out := []interface{}{}
+		for _, value := range v {
+			jsonValue, err := GetBSONValueAsJSON(value)
+			if err != nil {
+				return nil, err
+			}
+			out = append(out, jsonValue)
+		}
+		return out, nil
+
+	case string:
+		return v, nil // require no conversion
+
+	case int:
+		return json.NumberInt(v), nil
+
+	case bson.ObjectId: // ObjectId
+		return json.ObjectId(v.Hex()), nil
+
+	case time.Time: // Date
+		return json.Date(v.Unix()*1000 + int64(v.Nanosecond()/1e6)), nil
+
+	case int64: // NumberLong
+		return json.NumberLong(v), nil
+
+	case int32: // NumberInt
+		return json.NumberInt(v), nil
+
+	case float64:
+		return json.NumberFloat(v), nil
+
+	case float32:
+		return json.NumberFloat(float64(v)), nil
+
+	case []byte: // BinData (with generic type)
+		data := base64.StdEncoding.EncodeToString(v)
+		return json.BinData{0x00, data}, nil
+
+	case bson.Binary: // BinData
+		data := base64.StdEncoding.EncodeToString(v.Data)
+		return json.BinData{v.Kind, data}, nil
+
+	case mgo.DBRef: // DBRef
+		return json.DBRef{v.Collection, v.Id, v.Database}, nil
+
+	case bson.DBPointer: // DBPointer
+		return json.DBPointer{v.Namespace, v.Id}, nil
+
+	case bson.RegEx: // RegExp
+		return json.RegExp{v.Pattern, v.Options}, nil
+
+	case bson.MongoTimestamp: // Timestamp
+		timestamp := int64(v)
+		return json.Timestamp{
+			Seconds:   uint32(timestamp >> 32),
+			Increment: uint32(timestamp),
+		}, nil
+
+	case bson.JavaScript: // JavaScript
+		var scope interface{}
+		var err error
+		if v.Scope != nil {
+			scope, err = GetBSONValueAsJSON(v.Scope)
 			if err != nil {
 				return nil, err
 			}
