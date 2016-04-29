81c527a658a687b83564dfb9767df64420e9bcab
Gabriel Russell <gabriel.russell@mongodb.com>
2016-04-29 14:14:54 -0400

    Revert "TOOLS-1116 update servers.js to work with newer shells"
    This reverts commit 8f0496b4d4de16fd381cf7433a9b6f689a90e72b.

--- test/qa-tests/jstests/libs/servers.js
+++ test/qa-tests/jstests/libs/servers.js
@@ -869,7 +869,7 @@ startMongoProgram = function(){
     // Enable test commands.
     // TODO: Make this work better with multi-version testing so that we can support
     // enabling this on 2.4 when testing 2.6
-    var args = Array.from( arguments );
+    var args = argumentsToArray( arguments );
     args = appendSetParameterArgs(args);
     var pid = _startMongoProgram.apply( null, args );
 
@@ -896,7 +896,7 @@ startMongoProgram = function(){
 }
 
 runMongoProgram = function() {
-    var args = Array.from( arguments );
+    var args = argumentsToArray( arguments );
     var progName = args[0];
 
     if ( jsTestOptions().auth ) {
@@ -926,7 +926,7 @@ runMongoProgram = function() {
 // program name, and subsequent arguments to this function are passed as
 // command line arguments to the program.  Returns pid of the spawned program.
 startMongoProgramNoConnect = function() {
-    var args = Array.from( arguments );
+    var args = argumentsToArray( arguments );
     var progName = args[0];
 
     if ( jsTestOptions().auth ) {
