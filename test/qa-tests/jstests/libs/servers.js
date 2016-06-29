6a0d40326362fbd0f184082d49ac6e7b9bac1fd5
Zachary Snow <zach.snow@mongodb.com>
2016-06-29 12:33:50 -0400

    TOOLS-1269 sharding/replication test cleanup failures

--- test/qa-tests/jstests/libs/servers.js
+++ test/qa-tests/jstests/libs/servers.js
@@ -655,6 +655,7 @@ MongoRunner.runMongod = function( opts ){
     mongod.host = mongod.name
     mongod.port = parseInt( mongod.commandLine.port )
     mongod.runId = runId || ObjectId()
+    mongod.dbpath = fullOptions.dbpath;
     mongod.savedOptions = MongoRunner.savedOptions[ mongod.runId ];
     mongod.fullOptions = fullOptions;
     
