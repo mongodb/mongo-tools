ce355e9efbb54378d34ad15ae467a8ed32cb1568
Zachary Snow <zach.snow@mongodb.com>
2016-07-18 18:22:17 -0400

    fix stat_custom_headers test after humanReadable change

--- test/qa-tests/jstests/stat/stat_custom_headers.js
+++ test/qa-tests/jstests/stat/stat_custom_headers.js
@@ -13,6 +13,18 @@
   assert.eq(x, exitCodeBadOptions, "mongostat should fail with both -o and -O options");
   clearRawMongoProgramOutput();
 
+  // basic -o --humanReadable=false
+  x = runMongoProgram("mongostat", "--port", port,
+      "-o", "host,conn,time", "-n", 4, "--humanReadable=false");
+  assert.eq(x, 0, "mongostat should succeed with -o and -n options");
+  rows = statRows();
+  assert.eq(rows.length, 5, "expected 5 rows in mongostat output");
+  assert.eq(statFields(rows[0]).join(), "host,conn,time",
+      "first row doesn't match 'host conn time'");
+  assert.eq(statFields(rows[1]).length, 3,
+      "there should be exactly three entries for a row of this stat output");
+  clearRawMongoProgramOutput();
+
   // basic -o
   x = runMongoProgram("mongostat", "--port", port,
       "-o", "host,conn,time", "-n", 4);
@@ -21,8 +33,8 @@
   assert.eq(rows.length, 5, "expected 5 rows in mongostat output");
   assert.eq(statFields(rows[0]).join(), "host,conn,time",
       "first row doesn't match 'host conn time'");
-  assert.eq(statFields(rows[1]).length, 3,
-      "there should be exactly three entries for a row of this stat output");
+  assert.eq(statFields(rows[1]).length, 5,
+      "there should be exactly five entries for a row of this stat output (time counts as three)");
   clearRawMongoProgramOutput();
 
   // basic -O
@@ -43,17 +55,17 @@
   assert.eq(rows.length, 5, "expected 5 rows in mongostat output");
   assert.eq(statFields(rows[0]).join(), "H,C,MYTiME",
       "first row doesn't match 'H C MYTiME'");
-  assert.eq(statFields(rows[1]).length, 3,
-      "there should be exactly three entries for a row of this stat output");
+  assert.eq(statFields(rows[1]).length, 5,
+      "there should be exactly five entries for a row of this stat output (time counts as three)");
   clearRawMongoProgramOutput();
 
   // serverStatus custom field
   x = runMongoProgram("mongostat", "--port", port,
-      "-o", "host,time,mem.bits", "-n", 4);
+      "-o", "host,conn,mem.bits", "-n", 4);
   assert.eq(x, 0, "mongostat should succeed with -o and -n options");
   rows = statRows();
   assert.eq(rows.length, 5, "expected 5 rows in mongostat output");
-  assert.eq(statFields(rows[0]).join(), "host,time,mem.bits",
+  assert.eq(statFields(rows[0]).join(), "host,conn,mem.bits",
       "first row doesn't match 'host time mem.bits'");
   fields = statFields(rows[1]);
   assert.eq(fields.length, 3,
@@ -65,11 +77,11 @@
 
   // serverStatus named field
   x = runMongoProgram("mongostat", "--port", port,
-      "-o", "host,time=MYTiME,mem.bits=BiTs", "-n", 4);
+      "-o", "host,conn=MYCoNN,mem.bits=BiTs", "-n", 4);
   assert.eq(x, 0, "mongostat should succeed with -o and -n options");
   rows = statRows();
   assert.eq(rows.length, 5, "expected 5 rows in mongostat output");
-  assert.eq(statFields(rows[0]).join(), "host,MYTiME,BiTs",
+  assert.eq(statFields(rows[0]).join(), "host,MYCoNN,BiTs",
       "first row doesn't match 'host MYTiME BiTs'");
   fields = statFields(rows[1]);
   assert.eq(fields.length, 3,
