3779a3a7bd4e122099333a9ce4d305eb6570fa35
David Golden <xdg@xdg.me>
2017-11-01 17:03:22 -0400

    TOOLS-1822 Put copyright/license on all files
    This commit adds an Apache2 comment header to all .go files
    that lacked it.

--- common/options/options.go
+++ common/options/options.go
@@ -1,3 +1,9 @@
+// Copyright (C) MongoDB, Inc. 2014-present.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
 // Package options implements command-line options that are used by all of
 // the mongo tools.
 package options
