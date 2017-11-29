2daf6dc5abe0ce9e6527b3a0584be482eef92394
David Golden <xdg@xdg.me>
2017-11-29 09:28:51 -0500

    TOOLS-1878 Re-fix table scan test

--- test/qa-tests/jstests/export/force_table_scan.js
+++ test/qa-tests/jstests/export/force_table_scan.js
@@ -48,10 +48,10 @@
 
   // there should only be one query so far, and it should have snapshot set
   assert.eq(1, queries.length);
-  if (queries[0].command !== null) {
-    assert.eq(true, queries[0].command.snapshot);
-  } else {
+  if (queries[0].command === undefined) {
     assert.eq(true, queries[0].query.$snapshot || queries[0].query.snapshot);
+  } else {
+    assert.eq(true, queries[0].command.snapshot);
   }
 
   // remove the export file
@@ -72,10 +72,10 @@
   // there should be two queries, and the second one should not
   // have snapshot set
   assert.eq(2, queries.length);
-  if (queries[1].command !== null) {
-    assert.eq(true, !queries[1].command.snapshot);
-  } else {
+  if (queries[1].command === undefined) {
     assert(!queries[1].query['$snapshot']);
+  } else {
+    assert.eq(true, !queries[1].command.snapshot);
   }
 
   // wipe the collection
@@ -113,10 +113,10 @@
 
   // there should be 3 queries, and the last one should not have snapshot set
   assert.eq(3, queries.length);
-  if (queries[2].command !== null) {
-    assert.eq(true, !queries[2].command.snapshot);
-  } else {
+  if (queries[2].command === undefined) {
     assert(!queries[2].query['$snapshot']);
+  } else {
+    assert.eq(true, !queries[2].command.snapshot);
   }
 
 
