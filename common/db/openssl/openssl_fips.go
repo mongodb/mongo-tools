205d6c351d7c6da467e7329ec95eb983c5e9b911
Will Banfield <william.banfield@10gen.com>
2018-01-17 21:20:21 -0500

    TOOLS-1905 switch import paths for 10gen/openssl and revendor

--- common/db/openssl/openssl_fips.go
+++ common/db/openssl/openssl_fips.go
@@ -5,15 +5,20 @@
 // a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 
 // +build ssl
-// +build -darwin
+// +build !darwin
 
 package openssl
 
-import "github.com/spacemonkeygo/openssl"
+import (
+	"fmt"
+
+	"github.com/10gen/openssl"
+	"github.com/mongodb/mongo-tools/common/options"
+)
 
 func init() { sslInitializationFunctions = append(sslInitializationFunctions, SetUpFIPSMode) }
 
-func SetUpFIPSMode(opts *ToolOptions) error {
+func SetUpFIPSMode(opts options.ToolOptions) error {
 	if err := openssl.FIPSModeSet(opts.SSLFipsMode); err != nil {
 		return fmt.Errorf("couldn't set FIPS mode to %v: %v", opts.SSLFipsMode, err)
 	}
