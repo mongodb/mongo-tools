ef0186e3a8e748cdb0db4f4b47f134d65e095978
Mike Zraly <mike.zraly@sophos.com>
2018-04-10 20:55:04 -0400

    TOOLS-1997 verify float64 magnitude is small enough to cast to int64

--- mongostat/status/readers.go
+++ mongostat/status/readers.go
@@ -8,6 +8,7 @@ package status
 
 import (
 	"fmt"
+	"math"
 	"regexp"
 	"sort"
 	"time"
@@ -66,7 +67,9 @@ func numberToInt64(num interface{}) (int64, bool) {
 	case float32:
 		return int64(n), true
 	case float64:
-		return int64(n), true
+		if math.MinInt64 <= n && n <= math.MaxInt64 {
+			return int64(n), true
+		}
 	}
 	return 0, false
 }
