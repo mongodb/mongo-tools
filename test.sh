0682ec050288a282ce0ccd613272069d4b63ca1a
David Golden <xdg@xdg.me>
2018-11-14 15:37:21 -0500

    TOOLS-1996 Update test.sh script

--- test.sh
+++ test.sh
@@ -10,19 +10,41 @@ fi
 SCRIPT_DIR="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)"
 cd $SCRIPT_DIR
 
-sed -i.bak -e "s/built-without-version-string/$(git describe)/" \
-           -e "s/built-without-git-spec/$(git rev-parse HEAD)/" \
-           common/options/options.go
+VersionStr="$(git describe)"
+Gitspec="$(git rev-parse HEAD)"
+importpath="github.com/mongodb/mongo-tools/common/options"
+ldflags="-X ${importpath}.VersionStr=${VersionStr} -X ${importpath}.Gitspec=${Gitspec}"
 
 # remove stale packages
 rm -rf vendor/pkg
 
-. ./set_gopath.sh
-mkdir -p bin
+# build binaries for any tests that expect them for blackbox testing
+./build.sh
 
-for i in common bsondump mongostat mongofiles mongoexport mongoimport mongorestore mongodump mongotop mongoreplay; do
+ec=0
+
+# common can't recurse because we need different flags for different packages
+
+# Test common/db with test type flags
+for i in common/db; do
         echo "Testing ${i}..."
-        (cd $i && go test ./... -tags "${tags}")
+        (go test -ldflags "$ldflags" -tags "$tags" ./$i -test.types=unit,integration) || { echo "Error testing $i"; ec=1; }
 done
 
-mv -f common/options/options.go.bak common/options/options.go
+for i in common/db mongostat mongofiles mongoexport mongoimport mongorestore mongodump mongotop; do
+        echo "Testing ${i}..."
+        (cd $i && go test -ldflags "$ldflags" -tags "$tags" ./... -test.types=unit,integration) || { echo "Error testing $i"; ec=1; }
+done
+
+# These don't support the test.types flag
+common_with_test=$(find common -iname '*_test.go' | xargs -I % dirname % | sort -u | grep -v 'common/db')
+for i in bsondump mongoreplay $common_with_test; do
+        echo "Testing ${i}..."
+        (cd $i && go test -ldflags "$ldflags" -tags "$tags" . ) || { echo "Error testing $i"; ec=1; }
+done
+
+if [ -t /dev/stdin ]; then
+    stty sane
+fi
+
+exit $ec
