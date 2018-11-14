c4b473253a7bfc83101b64247f721a901f420d76
David Golden <xdg@xdg.me>
2018-11-14 15:35:32 -0500

    TOOLS-1742 Break circular dependency in db testing

--- common/db/connector_test.go
+++ common/db/connector_test.go
@@ -4,12 +4,13 @@
 // not use this file except in compliance with the License. You may obtain
 // a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 
-package db
+package db_test
 
 import (
 	"testing"
 	"time"
 
+	"github.com/mongodb/mongo-tools/common/db"
 	"github.com/mongodb/mongo-tools/common/options"
 	"github.com/mongodb/mongo-tools/common/testtype"
 	"github.com/mongodb/mongo-tools/common/testutil"
@@ -23,12 +24,12 @@ func TestVanillaDBConnector(t *testing.T) {
 
 	Convey("With a vanilla db connector", t, func() {
 
-		var connector *VanillaDBConnector
+		var connector *db.VanillaDBConnector
 
 		Convey("calling Configure should populate the addrs and dial timeout"+
 			" appropriately with no error", func() {
 
-			connector = &VanillaDBConnector{}
+			connector = &db.VanillaDBConnector{}
 
 			opts := options.ToolOptions{
 				Connection: &options.Connection{
@@ -38,21 +39,22 @@ func TestVanillaDBConnector(t *testing.T) {
 				Auth: &options.Auth{},
 			}
 			So(connector.Configure(opts), ShouldBeNil)
-			So(connector.dialInfo.Addrs, ShouldResemble,
+			info := db.VanillaTestWrapper(*connector).DialInfo()
+			So(info.Addrs, ShouldResemble,
 				[]string{"host1:20000", "host2:20000"})
-			So(connector.dialInfo.Timeout, ShouldResemble, time.Duration(opts.Timeout)*time.Second)
+			So(info.Timeout, ShouldResemble, time.Duration(opts.Timeout)*time.Second)
 
 		})
 
 		Convey("calling GetNewSession with a running mongod should connect"+
 			" successfully", func() {
 
-			connector = &VanillaDBConnector{}
+			connector = &db.VanillaDBConnector{}
 
 			opts := options.ToolOptions{
 				Connection: &options.Connection{
 					Host: "localhost",
-					Port: DefaultTestPort,
+					Port: db.DefaultTestPort,
 				},
 				Auth: &options.Auth{},
 			}
@@ -86,17 +88,17 @@ func TestVanillaDBConnectorWithAuth(t *testing.T) {
 	Convey("With a vanilla db connector and a mongod running with"+
 		" auth", t, func() {
 
-		var connector *VanillaDBConnector
+		var connector *db.VanillaDBConnector
 
 		Convey("connecting without authentication should not be able"+
 			" to run commands", func() {
 
-			connector = &VanillaDBConnector{}
+			connector = &db.VanillaDBConnector{}
 
 			opts := options.ToolOptions{
 				Connection: &options.Connection{
 					Host: "localhost",
-					Port: DefaultTestPort,
+					Port: db.DefaultTestPort,
 				},
 				Auth: &options.Auth{},
 			}
@@ -114,12 +116,12 @@ func TestVanillaDBConnectorWithAuth(t *testing.T) {
 		Convey("connecting with authentication should succeed and"+
 			" authenticate properly", func() {
 
-			connector = &VanillaDBConnector{}
+			connector = &db.VanillaDBConnector{}
 
 			opts := options.ToolOptions{
 				Connection: &options.Connection{
 					Host: "localhost",
-					Port: DefaultTestPort,
+					Port: db.DefaultTestPort,
 				},
 				Auth: &options.Auth{
 					Username: "cAdmin",
--- common/testutil/ssl_integration.go
+++ common/testutil/ssl_integration.go
@@ -8,10 +8,11 @@ package testutil
 
 import (
 	commonOpts "github.com/mongodb/mongo-tools/common/options"
+	"github.com/mongodb/mongo-tools/common/testtype"
 )
 
 func GetSSLOptions() commonOpts.SSL {
-	if HasTestType(SSLTestType) {
+	if testtype.HasTestType(testtype.SSLTestType) {
 		return commonOpts.SSL{
 			UseSSL:        true,
 			SSLCAFile:     "../common/db/openssl/testdata/ca.pem",
