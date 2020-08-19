4d78d723221d8009f0108d42f2327243b939d0bb
huan-Mongo <huan.li@mongodb.com>
2020-08-19 11:43:42 -0400

    address comments

--- test/qa-tests/jstests/top/mongotop_sharded.js
+++ test/qa-tests/jstests/top/mongotop_sharded.js
@@ -13,7 +13,7 @@ var testName = 'mongotop_sharded';
     jsTest.log('shell output: ' + shellOutput);
     shellOutput.split('\n').slice(1).forEach(function(line) {
       // check the displayed error message
-      assert.neq(line.match(expectedError), null, 'unexpeced error message');
+      assert.neq(line.match(expectedError), null, 'unexpected error message');
     });
   };
 
