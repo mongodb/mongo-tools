e51418a0807609a5b15adc87304861fa01644dea
huan-Mongo <huan.li@mongodb.com>
2020-09-09 00:04:57 -0400

    fix qa test

--- test/qa-tests/jstests/top/mongotop_json.js
+++ test/qa-tests/jstests/top/mongotop_json.js
@@ -23,19 +23,16 @@ var testName = 'mongotop_json';
 
     // ensure tool runs without error with --rowcount > 1
     var rowcount = 5;
-    if (jsTestOptions().useSSL) {
-      rowcount += 1;
-    }
     clearRawMongoProgramOutput();
     ret = executeProgram(['mongotop', '--port', conn.port, '--json', '--rowcount', rowcount].concat(passthrough.args));
     assert.eq(ret.exitCode, 0, 'failed 2');
     assert.eq.soon(rowcount, function() {
-      return ret.getOutput().split('\n').length;
+      var output = ret.getOutput().split('\n');
+      if (jsTestOptions().useSSL) {
+        output = output.slice(1);
+      }
+      return output.length;
     }, "expected " + rowcount + " top results");
-    var output = ret.getOutput().split('\n');
-    if (jsTestOptions().useSSL) {
-      output = output.slice(1);
-    }
     output.forEach(function(line) {
       assert(typeof JSON.parse(extractJSON(line)) === 'object', 'invalid JSON 2');
     });
