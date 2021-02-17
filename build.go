32ac638ff80d216b759301acf428802e86ed99a3
Evgeni Dobranov <evgeni.dobranov@mongodb.com>
2021-02-17 13:28:38 -0500

    TOOLS-2805: Add mod tidy static analysis check for Go modules (#348)

--- build.go
+++ build.go
@@ -12,7 +12,13 @@ import (
 var taskRegistry = task.NewRegistry(task.WithAutoNamespaces(true))
 
 func init() {
+	// Build
 	taskRegistry.Declare("build").Description("build the tools").OptionalArgs("tools").Do(buildscript.BuildTools)
+
+	// Static Analysis
+	taskRegistry.Declare("sa:modtidy").Description("runs go mod tidy").Do(buildscript.SAModTidy)
+
+	// Testing
 	taskRegistry.Declare("test:unit").Description("runs unit tests").OptionalArgs("tools").Do(buildscript.TestUnit)
 	taskRegistry.Declare("test:integration").Description("runs integration tests").OptionalArgs("tools", "ssl", "auth", "kerberos", "topology").Do(buildscript.TestIntegration)
 	taskRegistry.Declare("test:kerberos").Description("runs kerberos tests").Do(buildscript.TestKerberos)
