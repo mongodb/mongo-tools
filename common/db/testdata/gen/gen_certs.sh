6c41a5bab1c6f5c9bcc832bddc7fb8553376b7a9
Tim Fogarty <tim.fogarty@mongodb.com>
2021-05-26 18:27:14 +0200

    Update certs and add generation scriot

---
+++ common/db/testdata/gen/gen_certs.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -x
+
+openssl genrsa -out ../mongodb-test-ca.key 4096
+openssl req -batch -new -x509 -days 3650 -key ../mongodb-test-ca.key -out ../mongodb-test-ca.crt -config openssl-test-ca.cnf
+openssl genrsa -out ../mongodb-test-ia.key 4096
+openssl req -batch -new -key ../mongodb-test-ia.key -out ../mongodb-test-ia.csr -config openssl-test-ca.cnf
+openssl x509 -sha256 -req -days 3650 -in ../mongodb-test-ia.csr -CA ../mongodb-test-ca.crt -CAkey ../mongodb-test-ca.key -set_serial 01 -out ../mongodb-test-ia.crt -extfile openssl-test-ca.cnf -extensions v3_ca
+cat ../mongodb-test-ca.crt  > ../ca.pem
+cat ../mongodb-test-ia.crt  > ../ia.pem
+cat ../mongodb-test-ca.crt ../mongodb-test-ia.crt  > ../ca-ia.pem
+
+openssl genrsa -out ../mongodb-test-server1.key 4096
+openssl req -batch -new -key ../mongodb-test-server1.key -out ../mongodb-test-server1.csr -config openssl-test-server.cnf
+openssl x509 -sha256 -req -days 3650 -in ../mongodb-test-server1.csr -CA ../mongodb-test-ia.crt -CAkey ../mongodb-test-ia.key -CAcreateserial -out ../mongodb-test-server1.crt -extfile openssl-test-server.cnf -extensions v3_req
+cat ../mongodb-test-server1.crt ../mongodb-test-server1.key > ../test-server.pem
+
+openssl genrsa -out ../mongodb-test-client.key 4096
+openssl req -batch -new -key ../mongodb-test-client.key -out ../mongodb-test-client.csr -config openssl-test-client.cnf
+openssl x509 -sha256 -req -days 3650 -in ../mongodb-test-client.csr -CA ../mongodb-test-ia.crt -CAkey ../mongodb-test-ia.key -CAcreateserial -out ../mongodb-test-client.crt -extfile openssl-test-client.cnf -extensions v3_req
+cat ../mongodb-test-client.crt ../mongodb-test-client.key > ../test-client.pem
+
+openssl pkcs8 -v2 des3 -topk8 -passout pass:TOOLS_TESTING_PKCS8_PASSWORD -inform PEM -outform PEM -in ../test-client.pem -out ../test-client-pkcs8-encrypted.pem
+openssl pkcs8 -topk8 -nocrypt -inform PEM -outform PEM -in ../test-client.pem -out ../test-client-pkcs8-unencrypted.pem
