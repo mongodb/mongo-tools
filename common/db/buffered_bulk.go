e2842eb549308c16e070fb2e197d4631cfd085cb
Tim Fogarty <tim.fogarty@mongodb.com>
2022-01-27 14:57:14 +0100

    TOOLS-2875 Add batch byte limit to BufferedBulkInserter (#414)
    Co-authored-by: Evgeni Dobranov <evobranov@gmail.com>

--- common/db/buffered_bulk.go
+++ common/db/buffered_bulk.go
@@ -15,6 +15,10 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// The default value of maxMessageSizeBytes
+// See: https://docs.mongodb.com/manual/reference/command/hello/#mongodb-data-hello.maxMessageSizeBytes
+const MAX_MESSAGE_SIZE_BYTES = 48000000
+
 // BufferedBulkInserter implements a bufio.Writer-like design for queuing up
 // documents and inserting them in bulk when the given doc limit (or max
 // message size) is reached. Must be flushed at the end to ensure that all
@@ -24,6 +28,8 @@ type BufferedBulkInserter struct {
 	writeModels   []mongo.WriteModel
 	docLimit      int
 	docCount      int
+	byteCount     int
+	byteLimit     int
 	bulkWriteOpts *options.BulkWriteOptions
 	upsert        bool
 }
@@ -33,7 +39,11 @@ func newBufferedBulkInserter(collection *mongo.Collection, docLimit int, ordered
 		collection:    collection,
 		bulkWriteOpts: options.BulkWrite().SetOrdered(ordered),
 		docLimit:      docLimit,
-		writeModels:   make([]mongo.WriteModel, 0, docLimit),
+		// We set the byte limit to be slightly lower than maxMessageSizeBytes so it can fit in one OP_MSG.
+		// This may not always be perfect, e.g. we don't count update selectors in byte totals, but it should
+		// be good enough to keep memory consumption in check.
+		byteLimit:   MAX_MESSAGE_SIZE_BYTES - 100,
+		writeModels: make([]mongo.WriteModel, 0, docLimit),
 	}
 	return bb
 }
@@ -67,6 +77,7 @@ func (bb *BufferedBulkInserter) SetUpsert(upsert bool) *BufferedBulkInserter {
 func (bb *BufferedBulkInserter) resetBulk() {
 	bb.writeModels = bb.writeModels[:0]
 	bb.docCount = 0
+	bb.byteCount = 0
 }
 
 // Insert adds a document to the buffer for bulk insertion. If the buffer becomes full, the bulk write is performed, returning
@@ -83,18 +94,32 @@ func (bb *BufferedBulkInserter) Insert(doc interface{}) (*mongo.BulkWriteResult,
 // Update adds a document to the buffer for bulk update. If the buffer becomes full, the bulk write is performed, returning
 // any error that occurs.
 func (bb *BufferedBulkInserter) Update(selector, update bson.D) (*mongo.BulkWriteResult, error) {
-	return bb.addModel(mongo.NewUpdateOneModel().SetFilter(selector).SetUpdate(update).SetUpsert(bb.upsert))
+	rawBytes, err := bson.Marshal(update)
+	if err != nil {
+		return nil, err
+	}
+	bb.byteCount += len(rawBytes)
+
+	return bb.addModel(mongo.NewUpdateOneModel().SetFilter(selector).SetUpdate(rawBytes).SetUpsert(bb.upsert))
 }
 
 // Replace adds a document to the buffer for bulk replacement. If the buffer becomes full, the bulk write is performed, returning
 // any error that occurs.
 func (bb *BufferedBulkInserter) Replace(selector, replacement bson.D) (*mongo.BulkWriteResult, error) {
-	return bb.addModel(mongo.NewReplaceOneModel().SetFilter(selector).SetReplacement(replacement).SetUpsert(bb.upsert))
+	rawBytes, err := bson.Marshal(replacement)
+	if err != nil {
+		return nil, err
+	}
+	bb.byteCount += len(rawBytes)
+
+	return bb.addModel(mongo.NewReplaceOneModel().SetFilter(selector).SetReplacement(rawBytes).SetUpsert(bb.upsert))
 }
 
 // InsertRaw adds a document, represented as raw bson bytes, to the buffer for bulk insertion. If the buffer becomes full,
 // the bulk write is performed, returning any error that occurs.
 func (bb *BufferedBulkInserter) InsertRaw(rawBytes []byte) (*mongo.BulkWriteResult, error) {
+	bb.byteCount += len(rawBytes)
+
 	return bb.addModel(mongo.NewInsertOneModel().SetDocument(rawBytes))
 }
 
@@ -110,7 +135,7 @@ func (bb *BufferedBulkInserter) addModel(model mongo.WriteModel) (*mongo.BulkWri
 	bb.docCount++
 	bb.writeModels = append(bb.writeModels, model)
 
-	if bb.docCount >= bb.docLimit {
+	if bb.docCount >= bb.docLimit || bb.byteCount >= bb.byteLimit {
 		return bb.Flush()
 	}
 
