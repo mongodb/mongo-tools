d5fa1f2f129d43dc13c7c514486ad62ebcee7c71
Dave Rolsky <autarch@urth.org>
2022-11-09 14:15:34 -0600

    Use mlaunch for all cases where we start mongod or mongos in CI

--- scripts/run_mlaunch.sh
+++ scripts/run_mlaunch.sh
@@ -6,13 +6,33 @@ set -e
 
 echo "starting sharded cluster"
 
-if [ "${USE_TLS}" = "true" ]; then
-  MLAUNCH_ARGS="$MLAUNCH_ARGS_TLS"
+MLAUNCH_ARGS="--port 33333"
+
+if [ "$USE_SSL" = "true" ]; then
+    MLAUNCH_ARGS="$MLAUNCH_ARGS --sslMode requireSSL --sslCAFile common/db/testdata/ca-ia.pem --sslPEMKeyFile common/db/testdata/test-server.pem --sslClientCertificate common/db/testdata/test-client.pem --sslAllowInvalidCertificates"
+fi
+
+if [ "$USE_TLS" = "true" ]; then
+    MLAUNCH_ARGS="$MLAUNCH_ARGS --tlsMode requireTLS --tlsCAFile common/db/testdata/ca-ia.pem --tlsCertificateKeyFile common/db/testdata/test-server.pem --tlsClientCertificateKeyFile common/db/testdata/test-client.pem --tlsAllowInvalidCertificates"
+fi
+
+if [ "$AWS_AUTH" = "true" ]; then
+    MLAUNCH_ARGS="$MLAUNCH_ARGS --auth --setParameter authenticationMechanisms=MONGODB-AWS,SCRAM-SHA-256"
+fi
+
+if [ "$USE_AUTH" = "true" ]; then
+    MLAUNCH_ARGS="$MLAUNCH_ARGS --auth"
+fi
+
+if [ -n "$STORAGE_ENGINE" ]; then
+    MLAUNCH_ARGS="$MLAUNCH_ARGS --storageEngine $STORAGE_ENGINE"
 fi
 
 CLUSTER_TYPE="--replicaset --nodes 3"
 if [ -n "$MLAUNCH_SHARDED_CLUSTER" ]; then
     CLUSTER_TYPE="$CLUSTER_TYPE --sharded 3"
+elif [ -n "$MLAUNCH_SINGLE_NODE" ]; then
+    CLUSTER_TYPE="--single"
 fi
 
 # The ./bin directory contains our downloaded mongod and mongos.
