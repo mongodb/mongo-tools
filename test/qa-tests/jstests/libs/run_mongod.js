f6a93d2df47de8b25a97a30ab9c872232cc0672a
Jian Guan <61915096+tdq45gj@users.noreply.github.com>
2023-02-23 17:12:17 -0500

    fx

--- test/qa-tests/jstests/libs/run_mongod.js
+++ test/qa-tests/jstests/libs/run_mongod.js
@@ -1,14 +1,13 @@
 // Wrap whole file in a function to avoid polluting the global namespace
 (function () {
-    let oldRunMongod = MongoRunner.runMongod;
+  let oldRunMongod = MongoRunner.runMongod;
+  MongoRunner.runMongod = function(opts) {
+    print("MongoRunner.runMongod overriden in mongo-tools");
 
-    MongoRunner.runMongod = function(opts) {
-        print("MongoRunner.runMongod overriden in mongo-tools");
+    if (opts !== undefined && opts.journal !== undefined) {
+      delete opts.journal;
+    }
 
-        if (opts !== undefined && opts.journal !== undefined) {
-            delete opts.journal;
-        }
-
-        return oldRunMongod(opts);
-    };
+    return oldRunMongod(opts);
+  };
 })()
