2c9d6b21c40216541793a38b6f257964d3bb32e1
Jian Guan <61915096+tdq45gj@users.noreply.github.com>
2023-02-23 09:27:23 -0500

    Update run_mongod.js

--- test/legacy42/lib/run_mongod.js
+++ test/legacy42/lib/run_mongod.js
@@ -1,7 +1,8 @@
 /**
- * This file overrides MongoRunner.runMongod function in mongo shell to remove "journal" option before starting mongod
- * to make the deprecated mongo shell com
  * This file has to be loaded to mongo shell before loading js tests.
+ *
+ * This file overrides MongoRunner.runMongod function in mongo shell:
+ *  - Removes "journal" option before starting mongod to make the deprecated mongo shell compatible with MongoDB 6.1+
  *
  * MongoRunner.runMongod Starts a mongod instance.
  */
