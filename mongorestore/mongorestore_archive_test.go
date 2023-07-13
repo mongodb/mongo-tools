9877f340bf8b75417c8c7df5723d1bae79d291ee
Johnny DuBois <jddubois@umich.edu>
2023-07-13 12:39:15 -0400

    fix issue where collections containing 'admin' were ignored in non-admin databases

--- mongorestore/mongorestore_archive_test.go
+++ mongorestore/mongorestore_archive_test.go
@@ -7,11 +7,17 @@
 package mongorestore
 
 import (
+	"context"
+	"path/filepath"
+
 	"github.com/mongodb/mongo-tools/common/archive"
 	"github.com/mongodb/mongo-tools/common/log"
 	"github.com/mongodb/mongo-tools/common/options"
 	"github.com/mongodb/mongo-tools/common/testtype"
 	"github.com/mongodb/mongo-tools/common/testutil"
+	"github.com/stretchr/testify/require"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 
 	. "github.com/smartystreets/goconvey/convey"
 
@@ -131,4 +137,182 @@ func TestMongorestoreBadFormatArchive(t *testing.T) {
 			So(result.Successes, ShouldEqual, 0)
 		})
 	})
+}
+
+// ----------------------------------------------------------------------
+// All tests from this point onwards use testify, not convey. See the
+// CONTRIBUING.md file in the top level of the repo for details on how to
+// write tests using testify.
+// ----------------------------------------------------------------------
+
+func TestMongorestoreArchiveAdminNamespaces(t *testing.T) {
+	require := require.New(t)
+
+	testtype.SkipUnlessTestType(t, testtype.IntegrationTestType)
+
+	session, err := testutil.GetBareSession()
+	require.NoError(err, "can connect to server")
+
+	// TODO (Johnny) what is the correct FCV to use?
+	fcv := testutil.GetFCV(session)
+	if cmp, err := testutil.CompareFCV(fcv, "4.0"); err != nil || cmp < 0 {
+		t.Skipf("Requires server with FCV 4.0 or later and we have %s", fcv)
+	}
+
+	t.Run("restore from archive as atlas proxy", func(t *testing.T) {
+		testRestoreAdminNamespacesAsAtlasProxy(t)
+	})
+
+	t.Run("restore from archive as non atlas proxy", func(t *testing.T) {
+		testRestoreAdminNamespaces(t)
+	})
+}
+
+func testRestoreAdminNamespaces(t *testing.T) {
+	require := require.New(t)
+
+	session, err := testutil.GetBareSession()
+	require.NoError(err, "can connect to server")
+
+	dbName := uniqueDBName()
+	testDB := session.Database(dbName)
+	adminDB := session.Database("admin")
+	defer func() {
+		err = testDB.Drop(nil)
+		if err != nil {
+			t.Fatalf("Failed to drop test database: %v", err)
+		}
+	}()
+
+	collections := []*mongo.Collection{
+		createCollectionWithTestDocument(t, testDB, "admincollection"),
+		createCollectionWithTestDocument(t, adminDB, "admincollection"),
+		createCollectionWithTestDocument(t, testDB, "collection"),
+		createCollectionWithTestDocument(t, adminDB, "collection"),
+	}
+
+	withArchiveMongodump(t, func(file string) {
+
+		for _, collection := range collections {
+			err := collection.Drop(context.Background())
+			require.NoError(err, "can drop collection")
+		}
+
+		restore, err := getRestoreWithArgs(
+			DropOption,
+			ArchiveOption+"="+file,
+		)
+		require.NoError(err)
+		defer restore.Close()
+
+		result := restore.Restore()
+		require.NoError(result.Err, "can run mongorestore")
+		require.EqualValues(0, result.Failures, "mongorestore reports 0 failures")
+
+		for _, collection := range collections {
+			count, err := collection.CountDocuments(context.Background(), bson.M{})
+			require.NoError(err, "can count documents")
+			require.EqualValues(1, count, "found x documents")
+		}
+
+	})
+}
+
+func testRestoreAdminNamespacesAsAtlasProxy(t *testing.T) {
+	require := require.New(t)
+
+	session, err := testutil.GetBareSession()
+	require.NoError(err, "can connect to server")
+
+	dbName := uniqueDBName()
+	testDB := session.Database(dbName)
+	adminDB := session.Database("admin")
+	defer func() {
+		err = testDB.Drop(nil)
+		if err != nil {
+			t.Fatalf("Failed to drop test database: %v", err)
+		}
+	}()
+
+	testDBAdminCollection := createCollectionWithTestDocument(t, testDB, "admincollection")
+	adminDBAdminCollection := createCollectionWithTestDocument(t, adminDB, "admincollection")
+	testDBCollection := createCollectionWithTestDocument(t, testDB, "collection")
+	adminDBCollection := createCollectionWithTestDocument(t, adminDB, "collection")
+
+	withArchiveMongodump(t, func(file string) {
+
+		for _, collection := range []*mongo.Collection{
+			testDBAdminCollection,
+			adminDBAdminCollection,
+			testDBCollection,
+			adminDBCollection,
+		} {
+			err := collection.Drop(context.Background())
+			require.NoError(err, "can drop collection")
+		}
+
+		restore, err := getAtlasProxyRestoreWithArgs(
+			DropOption,
+			ArchiveOption+"="+file,
+		)
+		require.NoError(err)
+		defer restore.Close()
+
+		result := restore.Restore()
+		require.NoError(result.Err, "can run mongorestore")
+		require.EqualValues(0, result.Failures, "mongorestore reports 0 failures")
+
+		count, err := testDBAdminCollection.CountDocuments(context.Background(), bson.M{})
+		require.NoError(err, "can count documents")
+		require.EqualValues(1, count, "found x documents")
+
+		count, err = adminDBAdminCollection.CountDocuments(context.Background(), bson.M{})
+		require.NoError(err, "can count documents")
+		require.EqualValues(0, count, "found x documents")
+
+		count, err = testDBCollection.CountDocuments(context.Background(), bson.M{})
+		require.NoError(err, "can count documents")
+		require.EqualValues(1, count, "found x documents")
+
+		count, err = adminDBCollection.CountDocuments(context.Background(), bson.M{})
+		require.NoError(err, "can count documents")
+		require.EqualValues(0, count, "found x documents")
+
+	})
+}
+
+func createCollectionWithTestDocument(t *testing.T, db *mongo.Database, collectionName string) *mongo.Collection {
+	require := require.New(t)
+	collection := db.Collection(collectionName)
+	_, err := collection.InsertOne(
+		context.Background(),
+		bson.M{"key": "value"},
+	)
+	require.NoError(err, "can insert documents into collection")
+	return collection
+}
+
+func getAtlasProxyRestoreWithArgs(args ...string) (*MongoRestore, error) {
+	restore, err := getRestoreWithArgs(args...)
+	if err != nil {
+		return nil, err
+	}
+	restore.isAtlasProxy = true
+	return restore, nil
+}
+
+func withArchiveMongodump(t *testing.T, testCase func(string)) {
+	dir, cleanup := testutil.MakeTempDir(t)
+	defer cleanup()
+	file := filepath.Join(dir, "archive")
+	runArchiveMongodump(t, file)
+	testCase(file)
+}
+
+func runArchiveMongodump(t *testing.T, file string) string {
+	require := require.New(t)
+	runMongodumpWithArgs(t, ArchiveOption+"="+file)
+	_, err := os.Stat(file)
+	require.NoError(err, "dump created archive data file")
+	return file
 }
