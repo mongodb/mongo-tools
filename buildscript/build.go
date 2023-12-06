4f29eb03f695741dac7d0595dd780bdc3d76c37f
Jian Guan <61915096+tdq45gj@users.noreply.github.com>
2023-12-06 10:42:56 -0500

    bump minimum go version

--- buildscript/build.go
+++ buildscript/build.go
@@ -31,7 +31,7 @@ var pkgNames = []string{
 }
 
 // minimumGoVersion must be prefixed with v to be parsed by golang.org/x/mod/semver
-var minimumGoVersion = "v1.20.10"
+const minimumGoVersion = "v1.20.12"
 
 func CheckMinimumGoVersion(ctx *task.Context) error {
 	goVersionStr, err := runCmd(ctx, "go", "version")
