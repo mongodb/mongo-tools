0fbc142e724355944d67806e42ca216102433de2
dylrich <dylan.richardson@mongodb.com>
2024-03-01 16:39:58 +0000

    DEVPROD-2553: migrate to garasign for signing
    This commit removes our use of the legacy Notary service in favor of
    Garasign for both PGP signing as well as Authenticode signing. MacOS
    artifacts are still signed with MacOS Notary. Our signing code has been
    moved to a single script that branches based on the operating system we
    are signing for. Notably, this commit has removed signing for our RPMs.
    This was done because we were signing those RPMs with a now-EOL server
    key, which leads me to believe that no one was actually verifying these
    signatures in practice. Users would have had to magically know which key
    to use, and we have not be publicizing this information anywhere. If
    removal of RPM signing breaks someone's workflow, it can be added back
    in, though users can get signed RPMs directly from repo.mongodb.{com|org}
    instead.

---
+++ scripts/sign_artifacts.sh
@@ -0,0 +1,85 @@
+set -o errexit
+
+pgp_sign() {
+  file_name=$1
+  signature_name=$2
+
+  podman run \
+    --env-file=signing-envfile \
+    --rm \
+    -v $PWD:$PWD \
+    -w $PWD \
+    artifactory.corp.mongodb.com/release-tools-container-registry-local/garasign-gpg \
+    /bin/bash -c "gpgloader && gpg --yes -v --armor -o ${signature_name} --detach-sign ${file_name}"
+}
+
+authenticode_sign() {
+  file_name=$1
+
+  podman run \
+  --env-file=signing-envfile \
+  --rm \
+  -v $PWD:$PWD \
+  -w $PWD \
+  artifactory.corp.mongodb.com/release-tools-container-registry-local/garasign-jsign \
+  /bin/bash -c "jsign -a mongo-authenticode-2021 --replace --tsaurl http://timestamp.digicert.com -d SHA-256 ${file_name}"
+}
+
+setup_garasign_authentication() {
+  set +x
+
+  echo "${ARTIFACTORY_PASSWORD}" | podman login --password-stdin --username ${ARTIFACTORY_USERNAME} artifactory.corp.mongodb.com
+
+  echo "GRS_CONFIG_USER1_USERNAME=${GARASIGN_USERNAME}" >> "signing-envfile"
+  echo "GRS_CONFIG_USER1_PASSWORD=${GARASIGN_PASSWORD}" >> "signing-envfile"
+
+  set -x
+}
+
+macos_notarize_and_sign() {
+  set -o verbose
+
+  # untar the release package and get the package name
+  tar xvzf release.tgz
+  pkgname=$(ls | grep mongodb-database-tools)
+  rm release.tgz
+
+  # turn the untarred package into a zip
+  zip -r unsigned.zip "$pkgname"
+
+  curl -LO https://macos-notary-1628249594.s3.amazonaws.com/releases/client/v3.3.3/darwin_amd64.zip
+  unzip darwin_amd64.zip
+  chmod 0755 ./darwin_amd64/macnotary
+  ./darwin_amd64/macnotary -v
+
+  # The key id and secret were set as MACOS_NOTARY_KEY and MACOS_NOTARY_SECRET
+  # env vars from the expansions. The macnotary client will look for these env
+  # vars so we don't need to pass the credentials as CLI options.
+  ./darwin_amd64/macnotary \
+      --task-comment "signing the mongo-database-tools release" \
+      --task-id "$TASK_ID" \
+      --file "$PWD/unsigned.zip" \
+      --mode notarizeAndSign \
+      --url https://dev.macos-notary.build.10gen.cc/api \
+      --bundleId com.mongodb.mongotools \
+      --out-path "$PWD/release.zip"
+}
+
+case $MONGO_OS in
+  "osx")
+    macos_notarize_and_sign
+    ;;
+
+  "windows-64")
+    setup_garasign_authentication
+    authenticode_sign "release.msi"
+    pgp_sign "release.zip" "release.zip.sig"
+    ;;
+
+  *)
+    setup_garasign_authentication
+    pgp_sign "release.tgz" "release.tgz.sig"
+    ;;
+esac
+
+ls -la
