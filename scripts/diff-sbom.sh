d7f46e342f69a9f4c834c3f918ac138c24474105
Dave Rolsky <autarch@urth.org>
2024-05-31 12:02:25 +0800

    Share logic for checking SBOM files for diffs

---
+++ scripts/diff-sbom.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [ -z "$1" ]; then
+    echo "You must provide a path to an SBOM to diff"
+    exit 1
+fi
+
+# The silkbomb tool updates the timestamp and version field for the doc on
+# every run, even if nothing else changed.
+git diff --exit-code --ignore-matching-lines '"timestamp":\s+".+"|"version":\s+[0-9]+' "$1"
