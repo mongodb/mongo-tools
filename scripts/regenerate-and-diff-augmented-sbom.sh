01d908a5833cb5e8213f4c1f4547e59a65757cd1
Dave Rolsky <dave.rolsky@mongodb.com>
2024-06-04 21:09:15 +0800

    TOOLS-3550 Check that the Augmented SBOM file is up to date, not just that it exists (#663)
    This changes our Evergreen config so that we check that the Augmented SBOM is up to date when doing a release, as opposed to just checking that the file exists.
    
    This also renames the Augmented SBOM file to match the release tag style we use and the common extension for an SBOM.

---
+++ scripts/regenerate-and-diff-augmented-sbom.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+set -x
+
+TAG="$EVG_TRIGGERED_BY_TAG"
+if [ -z "$TAG" ]; then
+    echo "Cannot regenerate the Augmented SBOM file without a tag"
+    exit 1
+fi
+
+SBOM="ssdlc/$TAG.bom.json"
+if [ ! -f "$SBOM" ]; then
+    echo "The $SBOM does not exist at all"
+    exit 1
+fi
+
+./scripts/regenerate-augmented-sbom.sh --no-update-timestamp --no-update-sbom-version
+./scripts/diff-sbom.sh "$SBOM"
--- scripts/regenerate-and-diff-sbom.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-set -x
-
-./scripts/regenerate-sbom.sh
-git diff --exit-code --ignore-matching-lines '"timestamp":\s+".+"' cyclonedx.sbom.json
