323a0b0b4e39b5e4b10ae474b2a6b9fc7ad6e3ea
Dave Rolsky <dave.rolsky@mongodb.com>
2024-06-12 21:10:55 +0800

    TOOLS-3527 Make sure the Augmented SBOM has an empty `vulnerabilities` key if there are no vulnerabilities (#676)
    The `silkbomb` tool (or Silk?) should be doing this for us, but it's not. This PR uses `jq` to append this key if it's not present.
    
    This also fixes the `regenerate-augmented-sbom.sh` script to exit `0` if there is no tag. This script is run for the `main` branch as well as for tags.

--- scripts/diff-sbom.sh
+++ scripts/diff-sbom.sh
@@ -10,4 +10,6 @@ fi
 
 # The silkbomb tool updates the timestamp and version field for the doc on
 # every run, even if nothing else changed.
-git diff --exit-code "$1"
+#
+# TODO (TOOLS-3561): Remove the `--ignore-matching-lines` bit.
+git diff --ignore-matching-lines '"timestamp":\s+".+"|"version":\s+[0-9]+' --exit-code "$1"
