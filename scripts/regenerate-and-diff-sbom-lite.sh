ac9a24a5f97f8fbe9dc1d8e78abc70b28a132663
Dave Rolsky <dave.rolsky@mongodb.com>
2024-07-08 12:16:37 -0500

    Update Augmented SBOM for 100.9.5 to match what's in Silk (#690)
    This updates our Augmented SBOM for 100.9.5 to match what's in Silk.
    
    it also changes the SBOM scripts to not ignore diffs in the timestamp & version. If these fields _do_ change then they should change. From discussions with DevProd, there's no bug in `silkbomb`.

--- scripts/regenerate-and-diff-sbom-lite.sh
+++ scripts/regenerate-and-diff-sbom-lite.sh
@@ -4,4 +4,4 @@ set -e
 set -x
 
 ./scripts/regenerate-sbom-lite.sh --no-update-timestamp --no-update-sbom-version
-./scripts/diff-sbom.sh cyclonedx.sbom.json
+git diff --exit-code cyclonedx.sbom.json
