6036fb2d040f26cf11e27fe484676162dfa0ff77
Jian Guan <61915096+tdq45gj@users.noreply.github.com>
2024-07-22 16:51:52 -0400

    Update regenerate-and-diff-sbom-lite.sh

--- scripts/regenerate-and-diff-sbom-lite.sh
+++ scripts/regenerate-and-diff-sbom-lite.sh
@@ -4,4 +4,6 @@ set -e
 set -x
 
 ./scripts/regenerate-sbom-lite.sh --no-update-timestamp --no-update-sbom-version
-git diff --exit-code cyclonedx.sbom.json
+
+# TODO (TOOLS-3621): Check the entire file once DEVPROD-9074 is fixed.
+git diff --exit-code cyclonedx.sbom.json --ignore-matching-lines='timestamp' --ignore-matching-lines='bom-ref'
