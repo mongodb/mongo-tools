0b168a3f5ec8f8aebdeead23ed1be6cefe2ced1b
Patrick Meredith <pmeredit@protonmail.com>
2025-01-09 15:11:38 -0500

    TOOLS-3736: Used updateAllDeps, we'll see

--- vendor/github.com/smarty/assertions/doc.go
+++ vendor/github.com/smarty/assertions/doc.go
@@ -87,11 +87,8 @@ func (this *Assertion) So(actual any, assert SoFunc, expected ...any) bool {
 // For an alternative implementation of So (that provides more flexible return options)
 // see the `So` function in the package at github.com/smarty/assertions/assert.
 func So(actual any, assert SoFunc, expected ...any) (bool, string) {
-	if result := so(actual, assert, expected...); len(result) == 0 {
-		return true, result
-	} else {
-		return false, result
-	}
+	result := so(actual, assert, expected...)
+	return len(result) == 0, result
 }
 
 // so is like So, except that it only returns the string message, which is blank if the
