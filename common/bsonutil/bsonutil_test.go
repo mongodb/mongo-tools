4b64a120ec202d6bc48ad4a540dacf6ebf752b40
janet.io <96701821+janetio@users.noreply.github.com>
2025-01-30 09:15:21 -0500

    TOOLS-3637 Mongodump should check collection metadata consistency (#741)
    Mongodump should error if metadata that is marshalled from BSON to
    ExtJSON is inconsistent when it is unmarshaled again since this will
    result in inconsistencies between mongodump and mongorestore.

--- common/bsonutil/bsonutil_test.go
+++ common/bsonutil/bsonutil_test.go
@@ -1,6 +1,7 @@
 package bsonutil
 
 import (
+	"math"
 	"testing"
 	"time"
 
@@ -104,34 +105,78 @@ func TestMarshalExtJSONReversible(t *testing.T) {
 
 	tests := []struct {
 		val          any
-		canonical    bool
 		reversible   bool
 		expectedJSON string
 	}{
 		{
 			bson.M{"field1": bson.M{"$date": 1257894000000}},
 			true,
-			true,
 			`{"field1":{"$date":{"$numberLong":"1257894000000"}}}`,
 		},
 		{
 			bson.M{"field1": time.Unix(1257894000, 0)},
 			true,
-			true,
 			`{"field1":{"$date":{"$numberLong":"1257894000000"}}}`,
 		},
 		{
 			bson.M{"field1": bson.M{"$date": "invalid"}},
-			true,
 			false,
 			``,
 		},
 	}
 
 	for _, test := range tests {
-		json, err := MarshalExtJSONReversible(test.val, test.canonical, false)
+		json, err := MarshalExtJSONReversible(
+			test.val,
+			true,  /* canonical */
+			false, /* escapeHTML */
+		)
 		if !test.reversible {
 			assert.ErrorContains(t, err, "marshal is not reversible")
+		} else {
+			assert.NoError(t, err)
+		}
+		assert.Equal(t, test.expectedJSON, string(json))
+	}
+}
+
+func TestMarshalExtJSONWithBSONRoundtripConsistency(t *testing.T) {
+	testtype.SkipUnlessTestType(t, testtype.UnitTestType)
+
+	tests := []struct {
+		val                          any
+		consistentAfterRoundtripping bool
+		expectedJSON                 string
+	}{
+		{
+			bson.M{"field1": bson.M{"grapes": int64(123)}},
+			true,
+			`{"field1":{"grapes":{"$numberLong":"123"}}}`,
+		},
+		{
+			bson.M{"field1": bson.M{"$date": 1257894000000}},
+			false,
+			``,
+		},
+		{
+			bson.M{"field1": bson.M{"nanField": math.NaN()}},
+			true,
+			`{"field1":{"nanField":{"$numberDouble":"NaN"}}}`,
+		},
+	}
+
+	for _, test := range tests {
+		json, err := MarshalExtJSONWithBSONRoundtripConsistency(
+			test.val,
+			true,  /* canonical */
+			false, /* escapeHTML */
+		)
+		if !test.consistentAfterRoundtripping {
+			assert.ErrorContains(
+				t,
+				err,
+				"marshaling BSON to ExtJSON and back resulted in discrepancies",
+			)
 		} else {
 			assert.NoError(t, err)
 		}
