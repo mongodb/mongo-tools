e434a2f40048ab9bac7681671054361dbc07868e
shyam <shyam12860@gmail.com>
2025-01-31 16:47:43 -0600

    Handle 'system.buckets' prefix correctly when .bson is target
    When the bson file is passed as the target, CreateIntentForCollection
    does not set the collection name correctly by removing the
    "system.buckets." prefix. This will cause errors like:
    "Invalid namespace: test.system.buckets.system.buckets" as a result of
    adding the same prefix twice to the namespace.
    
    Making this change in the processing of the file so that the intent can
    be initialized correctly rather than remove the prefix in the intent, so
    that all code that intentionally accesses the collection name without
    the prefix (when dropping the collection for example) works correctly.

--- mongorestore/filepath_test.go
+++ mongorestore/filepath_test.go
@@ -457,6 +457,64 @@ func TestCreateIntentsForCollection(t *testing.T) {
 			})
 		})
 
+		Convey("running CreateIntentForCollection on a system.buckets file *with* metadata", func() {
+			ddl, err := newActualPath(util.ToUniversalPath("testdata/testdirs/db1/system.buckets.c5timeseries.bson"))
+			So(err, ShouldBeNil)
+			mr.ToolOptions.Namespace = &commonOpts.Namespace{}
+
+			err = mr.handleBSONInsteadOfDirectory(ddl.Path())
+			So(err, ShouldBeNil)
+			So(mr.ToolOptions.Namespace.DB, ShouldEqual, "db1")
+			So(mr.ToolOptions.Namespace.Collection, ShouldEqual, "system.buckets.c5timeseries")
+
+			Convey("should create one intent with inferred fields from BSON name", func() {
+				err = mr.CreateIntentForCollection(mr.ToolOptions.Namespace.DB, mr.ToolOptions.Namespace.Collection, ddl)
+				mr.manager.Finalize(intents.Legacy)
+				So(err, ShouldBeNil)
+
+				i0 := mr.manager.Pop()
+				So(i0, ShouldNotBeNil)
+				So(i0.DB, ShouldEqual, mr.ToolOptions.Namespace.DB)
+				So(i0.C, ShouldEqual, "c5timeseries")
+				So(i0.Location, ShouldEqual, util.ToUniversalPath(ddl.Path()))
+				i1 := mr.manager.Pop()
+				So(i1, ShouldBeNil)
+
+				Convey("and a set Metadata path", func() {
+					So(
+						i0.MetadataLocation,
+						ShouldEqual,
+						util.ToUniversalPath("testdata/testdirs/db1/c5timeseries.metadata.json"),
+					)
+					logs := buff.String()
+					So(strings.Contains(logs, "found metadata"), ShouldEqual, true)
+				})
+			})
+
+			Convey("should create an intent correctly when input db and collection params contain 'system.buckets.' prefix", func() {
+				err = mr.CreateIntentForCollection("myDB", "system.buckets.myC", ddl)
+				mr.manager.Finalize(intents.Legacy)
+				So(err, ShouldBeNil)
+				i0 := mr.manager.Pop()
+				So(i0, ShouldNotBeNil)
+				So(i0.DB, ShouldEqual, "myDB")
+				So(i0.C, ShouldEqual, "myC")
+				So(i0.Location, ShouldEqual, ddl.Path())
+				i1 := mr.manager.Pop()
+				So(i1, ShouldBeNil)
+
+				Convey("and a set Metadata path", func() {
+					So(
+						i0.MetadataLocation,
+						ShouldEqual,
+						util.ToUniversalPath("testdata/testdirs/db1/c5timeseries.metadata.json"),
+					)
+					logs := buff.String()
+					So(strings.Contains(logs, "found metadata"), ShouldEqual, true)
+				})
+			})
+		})
+
 	})
 }
 
