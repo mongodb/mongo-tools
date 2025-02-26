918158a1aed4248cd800ad286a20c1c5f05db3c6
Felipe Gasper <felipe.gasper@mongodb.com>
2025-02-26 14:51:13 -0500

    drop pre-4.2 tests

--- common/db/db.go
+++ common/db/db.go
@@ -330,7 +330,7 @@ func configureClient(opts options.ToolOptions) (*mongo.Client, error) {
 	clientopt := mopt.Client()
 	cs := opts.URI.ParsedConnString()
 
-	clientopt.Hosts = cs.Hosts
+	clientopt.ApplyURI(opts.URI.ConnectionString)
 
 	if opts.RetryWrites != nil {
 		clientopt.SetRetryWrites(*opts.RetryWrites)
