0655e30e7fe6430bb437cc7072c1a64e32bbb408
shyam <shyam12860@gmail.com>
2025-03-19 11:17:24 -0500

    changes to allow 8.1 libs

--- test/shell_common/libs/load_libs-8.1.js
+++ test/shell_common/libs/load_libs-8.1.js
@@ -1,11 +1,13 @@
 // All changes required to run JS tests in legacy / qa-tests directories should go in this file.
 // Assume that the shell is running from either test/legacy42/ or test/qa-tests directories for load paths.
 
-// Just load 8.0's replsettest, our legacy shell tests haven't changed in forever, so it shouldn't matter.
 TestData = new Object();
 
-const {ReplSetTest} = await import('../shell_common/libs/replsettest-8.0.js');
+const {ReplSetTest} = await import('../shell_common/libs/replsettest-8.1.js');
 globalThis.ReplSetTest = ReplSetTest
+
+const {ShardingTest} = await import('../shell_common/libs/shardingtest-8.1.js');
+globalThis.ShardingTest = ShardingTest
 
 // in 8.1 shell rawMongoProgramOutput expects one parameter. Change it here specifically when running from 8.1 shell.
 var __origRawMongoProgramOutput = rawMongoProgramOutput;
