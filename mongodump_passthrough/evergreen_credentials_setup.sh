8ffb72b16cb6b8074401978c8640f63aaea1a516
Richard Cownie <richard.cownie@mongodb.com>
2025-04-09 12:53:34 -0400

    remove spurious shell scripts

--- mongodump_passthrough/evergreen_credentials_setup.sh
+++ mongodump_passthrough/evergreen_credentials_setup.sh
@@ -3,6 +3,7 @@
 set -o errexit
 set -o verbose
 
+# TODO: Remove this once we have the right private variable in evergreen project settings.
 evergreen_api_key=823845634ee1a616b7702cc1e24df96e
 evergreen_api_user=richard.cownie
 
@@ -13,6 +14,3 @@ api_server_host: https://evergreen.mongodb.com/api
 api_key: "$evergreen_api_key"
 user: "$evergreen_api_user"
 END_OF_CREDS
-
-cp .evergreen.yml src/mongosync
-
