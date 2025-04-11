65dbf3efec30be67f82e8e8ff64a48496b0dff62
Richard Cownie <richard.cownie@mongodb.com>
2025-04-11 11:20:37 -0400

    fix

--- mongodump_passthrough/evergreen_credentials_setup.sh
+++ mongodump_passthrough/evergreen_credentials_setup.sh
@@ -3,10 +3,6 @@
 set -o errexit
 set -o verbose
 
-# TODO: Remove this once we have the right private variable in evergreen project settings.
-evergreen_api_key=823845634ee1a616b7702cc1e24df96e
-evergreen_api_user=richard.cownie
-
 # Create the Evergreen API credentials
 # shellcheck disable=SC2154
 cat >.evergreen.yml <<END_OF_CREDS
