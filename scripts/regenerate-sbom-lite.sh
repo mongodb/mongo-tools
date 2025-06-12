5cd467f3f368efcf52a728384aa2a5530dd1e02d
Dave Rolsky <dave.rolsky@mongodb.com>
2025-06-12 09:46:40 -0500

    TOOLS-3903 Always include all deps for all platforms in SBOM and only include deps for released binaries (#810)
    Previously, the SBOM would only contain dependencies that were used in
    the platform on which we generated the SBOM Lite file. We didn't notice
    this because we did not have any platform-conditional dependencies, but
    Jane was working on a PR that did add such a dep.
    
    This changes the code that generates the SBOM Lite to get a list of deps
    for each of our supported platforms.
    
    This _also_ changes the SBOM generation to only look at dependencies of
    the code for each of the release binaries. Previously, we included all
    the deps for everything in the repo. There were a huge number of
    packages that were only used by dev tooling, but not in the released
    tools.

--- scripts/regenerate-sbom-lite.sh
+++ scripts/regenerate-sbom-lite.sh
@@ -1,22 +1,46 @@
 #!/bin/bash
 
 set -e
+set -o pipefail
 set -x
 
-# This set of piped commands generates a file that contains each dependency as
-# a purl (https://github.com/package-url/purl-spec), one per line. This is
-# used as input for the `silkbomb` tool to generate an SBOM.
-go list -json -mod=mod all |
-  jq -r '.Module // empty | "pkg:golang/" + (.Path // empty) + "@" + (.Version // empty)' |
-    sort -u >purls.txt
+rm -f purls.txt
+
+BINARY_DIRS="$( go run release/release.go print-binary-paths )"
+OS_ARCH_COMBOS="$( go run release/release.go print-os-arch-combos )"
+
+# This set of piped commands generates a file that contains each dependency as a purl
+# (https://github.com/package-url/purl-spec), one per line. This is used as input for the `silkbomb`
+# tool to generate an SBOM. We do this for each OS/architecture combination we support to make sure
+# this is the superset of all our dependencies.
+#
+# shellcheck disable=SC2086 # we intentionally don't quote `$OS_ARCH_COMBOS` so we split on the
+# whitespace.
+for c in $OS_ARCH_COMBOS; do
+    os="$(echo $c | cut -f1 -d/)"
+    arch="$(echo $c | cut -f2 -d/)"
+    # shellcheck disable=SC2086 # we don't want to quote `$BINARY_DIRS` for the same reason.
+    GOOS="$os" GOARCH="$arch" go list -json -mod=mod -deps $BINARY_DIRS |
+        jq -r '.Module // empty | "pkg:golang/" + .Path + "@" + .Version // empty' >> \
+            purls.txt
+done
+
+sort -u -o purls.txt purls.txt
+
+if [ ! -s purls.txt ]; then
+    echo 'The purls.txt file generated from the "go list" output is empty!'
+    exit 1
+fi
+
 go version |
     sed 's|^go version \([^ ]*\) *.*|pkg:golang/std@\1|' >>purls.txt
+
 # The arguments to the silkbomb program start at "update".
 #
-# shellcheck disable=SC2068 # we don't want to quote `$@`
+# shellcheck disable=SC2068 # we don't want to quote `$@`.
 podman run \
-    -it \
     --rm \
+    --platform linux/amd64 \
     -v "${PWD}":/pwd \
     artifactory.corp.mongodb.com/release-tools-container-registry-public-local/silkbomb:2.0 \
     update \
