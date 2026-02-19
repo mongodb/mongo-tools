b1c8e03e394a67f59ccb7d85089338a622d22482
Michael McClimon <michael.mcclimon@mongodb.com>
2026-02-19 11:05:26 -0500

    common/archive: rewrite convey tests in testify

--- common/archive/multiplexer_roundtrip_test.go
+++ common/archive/multiplexer_roundtrip_test.go
@@ -18,9 +18,10 @@ import (
 	"github.com/mongodb/mongo-tools/common/db"
 	"github.com/mongodb/mongo-tools/common/intents"
 	"github.com/mongodb/mongo-tools/common/testtype"
-	. "github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.mongodb.org/mongo-driver/v2/bson"
+	"golang.org/x/sync/errgroup"
 )
 
 const testDocCount = 10000
@@ -70,130 +71,118 @@ func TestBasicMux(t *testing.T) {
 
 	var err error
 
-	Convey("with thousands of docs in each of five collections", t, func() {
-		buf := &closingBuffer{bytes.Buffer{}}
+	buf := &closingBuffer{bytes.Buffer{}}
 
-		mux := NewMultiplexer(buf, new(testNotifier))
-		muxIns := map[string]*MuxIn{}
+	mux := NewMultiplexer(buf, new(testNotifier))
+	muxIns := map[string]*MuxIn{}
 
-		inChecksum := map[string]hash.Hash{}
-		inLengths := map[string]*int{}
-		outChecksum := map[string]hash.Hash{}
-		outLengths := map[string]*int{}
+	inChecksum := map[string]hash.Hash{}
+	inLengths := map[string]*int{}
+	outChecksum := map[string]hash.Hash{}
+	outLengths := map[string]*int{}
 
-		// To confirm that what we multiplex is the same as what we demultiplex, we
-		// create input and output hashes for each namespace. After we finish
-		// multiplexing and demultiplexing we will compare all of the CRCs for each
-		// namespace
-		errChan := make(chan error)
-		makeIns(testIntents, mux, inChecksum, muxIns, inLengths, errChan)
+	// To confirm that what we multiplex is the same as what we demultiplex, we
+	// create input and output hashes for each namespace. After we finish
+	// multiplexing and demultiplexing we will compare all of the CRCs for each
+	// namespace
+	errChan := make(chan error)
+	makeIns(testIntents, mux, inChecksum, muxIns, inLengths, errChan)
 
-		Convey("each document should be multiplexed", func() {
-			go mux.Run()
+	t.Run("each document should be multiplexed", func(t *testing.T) {
+		go mux.Run()
 
-			for range testIntents {
-				err := <-errChan
-				So(err, ShouldBeNil)
-			}
-			close(mux.Control)
-			err = <-mux.Completed
-			So(err, ShouldBeNil)
-
-			demux := &Demultiplexer{
-				In:              buf,
-				NamespaceStatus: make(map[string]int),
-			}
-			demuxOuts := map[string]*RegularCollectionReceiver{}
-
-			errChan := make(chan error)
-			makeOuts(testIntents, demux, outChecksum, demuxOuts, outLengths, errChan)
-
-			Convey("and demultiplexed successfully", func() {
-				So(demux.Run(), ShouldBeNil)
-				So(err, ShouldBeNil)
-
-				for range testIntents {
-					err := <-errChan
-					So(err, ShouldBeNil)
-				}
-				for _, dbc := range testIntents {
-					ns := dbc.Namespace()
-					So(*inLengths[ns], ShouldEqual, *outLengths[ns])
-					inSum := inChecksum[ns].Sum([]byte{})
-					outSum := outChecksum[ns].Sum([]byte{})
-					So(inSum, ShouldResemble, outSum)
-				}
-			})
-		})
+		for range testIntents {
+			err := <-errChan
+			require.NoError(t, err)
+		}
+		close(mux.Control)
+		err = <-mux.Completed
+		require.NoError(t, err)
 	})
-	return
-}
 
-func TestParallelMux(t *testing.T) {
-	testtype.SkipUnlessTestType(t, testtype.UnitTestType)
-
-	Convey("parallel mux/demux over a pipe", t, func() {
-		readPipe, writePipe, err := os.Pipe()
-		So(err, ShouldBeNil)
-
-		mux := NewMultiplexer(writePipe, new(testNotifier))
-		muxIns := map[string]*MuxIn{}
-
+	t.Run("and demultiplexed successfully", func(t *testing.T) {
 		demux := &Demultiplexer{
-			In:              readPipe,
+			In:              buf,
 			NamespaceStatus: make(map[string]int),
 		}
 		demuxOuts := map[string]*RegularCollectionReceiver{}
 
-		inChecksum := map[string]hash.Hash{}
-		inLengths := map[string]*int{}
+		errChan := make(chan error)
+		makeOuts(testIntents, demux, outChecksum, demuxOuts, outLengths, errChan)
 
-		outChecksum := map[string]hash.Hash{}
-		outLengths := map[string]*int{}
-
-		writeErrChan := make(chan error)
-		readErrChan := make(chan error)
-
-		makeIns(testIntents, mux, inChecksum, muxIns, inLengths, writeErrChan)
-		makeOuts(testIntents, demux, outChecksum, demuxOuts, outLengths, readErrChan)
-
-		var (
-			wg       sync.WaitGroup
-			demuxErr error
-		)
-		wg.Add(1)
-		go func() {
-			demuxErr = demux.Run()
-			wg.Done()
-		}()
-		wg.Add(1)
-		go func() {
-			mux.Run()
-			wg.Done()
-		}()
+		err := demux.Run()
+		require.NoError(t, err)
 
 		for range testIntents {
-			err := <-writeErrChan
-			So(err, ShouldBeNil)
-			err = <-readErrChan
-			So(err, ShouldBeNil)
+			err := <-errChan
+			require.NoError(t, err)
 		}
-		close(mux.Control)
-		muxErr := <-mux.Completed
-		So(muxErr, ShouldBeNil)
-
 		for _, dbc := range testIntents {
 			ns := dbc.Namespace()
-			So(*inLengths[ns], ShouldEqual, *outLengths[ns])
+			assert.Equal(t, *inLengths[ns], *outLengths[ns])
 			inSum := inChecksum[ns].Sum([]byte{})
 			outSum := outChecksum[ns].Sum([]byte{})
-			So(inSum, ShouldResemble, outSum)
+			assert.Equal(t, inSum, outSum)
 		}
-
-		wg.Wait()
-		So(demuxErr, ShouldBeNil)
 	})
-	return
+
+}
+
+func TestParallelMuxOverPipe(t *testing.T) {
+	testtype.SkipUnlessTestType(t, testtype.UnitTestType)
+
+	readPipe, writePipe, err := os.Pipe()
+	require.NoError(t, err)
+
+	mux := NewMultiplexer(writePipe, new(testNotifier))
+	muxIns := map[string]*MuxIn{}
+
+	demux := &Demultiplexer{
+		In:              readPipe,
+		NamespaceStatus: make(map[string]int),
+	}
+	demuxOuts := map[string]*RegularCollectionReceiver{}
+
+	inChecksum := map[string]hash.Hash{}
+	inLengths := map[string]*int{}
+
+	outChecksum := map[string]hash.Hash{}
+	outLengths := map[string]*int{}
+
+	writeErrChan := make(chan error)
+	readErrChan := make(chan error)
+
+	makeIns(testIntents, mux, inChecksum, muxIns, inLengths, writeErrChan)
+	makeOuts(testIntents, demux, outChecksum, demuxOuts, outLengths, readErrChan)
+
+	var eg errgroup.Group
+
+	eg.Go(demux.Run)
+	eg.Go(func() error {
+		mux.Run()
+		return nil
+	})
+
+	for range testIntents {
+		err := <-writeErrChan
+		require.NoError(t, err)
+		err = <-readErrChan
+		require.NoError(t, err)
+	}
+	close(mux.Control)
+	muxErr := <-mux.Completed
+	require.NoError(t, muxErr)
+
+	for _, dbc := range testIntents {
+		ns := dbc.Namespace()
+		assert.Equal(t, *inLengths[ns], *outLengths[ns])
+		inSum := inChecksum[ns].Sum([]byte{})
+		outSum := outChecksum[ns].Sum([]byte{})
+		assert.Equal(t, inSum, outSum)
+	}
+
+	demuxErr := eg.Wait()
+	require.NoError(t, demuxErr)
 }
 
 func makeIns(
@@ -261,7 +250,11 @@ func makeOuts(
 		demuxOuts[ns] = muxOut
 		outLengths[ns] = &outLength
 
-		So(demuxOuts[ns].Open(), ShouldBeNil)
+		err := demuxOuts[ns].Open()
+		if err != nil {
+			// TODO
+			panic(err)
+		}
 		go func() {
 			bs := make([]byte, db.MaxBSONSize)
 			var err error
