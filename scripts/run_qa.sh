1ec52f42bfde5bcdd8af746226d7e95928bcad83
Michael McClimon <michael.mcclimon@mongodb.com>
2026-03-23 13:58:21 -0400

    TOOLS-4141 windows: use correct python/pip paths for qa-tests (#929)
    This broke when I upgraded us to the newer windows distro in 862b2f2c
    (TOOLS-4127 Add 8.3 support to test matrix, 2026-03-20). Note that the
    aws-auth tests are still broken on Windows with this commit; a fix for
    that will come shortly.

--- scripts/run_qa.sh
+++ scripts/run_qa.sh
@@ -10,13 +10,11 @@ cd test/qa-tests
 chmod 400 jstests/libs/key*
 
 PATH=/opt/mongodbtoolchain/v3/bin/:$PATH
-python="python3"
-if [ "Windows_NT" = "$OS" ]; then
-    python="py.exe -3"
-fi
+python="${python_binary:-python3}"
+
 $python -m venv venv
-pip3 install pymongo==3.12.1 pyyaml
+$python -m pip install pymongo==3.12.1 pyyaml
 if [ "Windows_NT" = "$OS" ]; then
-    pip3 install pywin32
+    $python -m pip install pywin32
 fi
 $python buildscripts/resmoke.py --suite=${resmoke_suite} --continueOnFailure --log=buildlogger --reportFile=../../report.json ${resmoke_args} --excludeWithAnyTags="${excludes}"
