fd4d32d09694db27f993fed90ea3c6ca3dcc7f96
Matthew Brazel <matthew.brazel@mongodb.com>
2026-07-15 12:05:34 +0100

    TOOLS-4248, TOOLS-4253; Add support for 7.0,8.0 enterprise on rhel10 (s390x) (#1028)

--- release/platform/platform.go
+++ release/platform/platform.go
@@ -556,6 +556,15 @@ var platforms = []Platform{
 	},
 	{
 		Name:                  "rhel10",
+		Arch:                  ArchS390x,
+		OS:                    OSLinux,
+		Pkg:                   PkgRPM,
+		Repos:                 []Repo{RepoEnterprise},
+		BuildTags:             defaultBuildTags,
+		MinLinuxServerVersion: &version.Version{Major: 7, Minor: 0, Patch: 0},
+	},
+	{
+		Name:                  "rhel10",
 		Arch:                  ArchX86_64,
 		OS:                    OSLinux,
 		Pkg:                   PkgRPM,
