6c39fd4493db59c06b3fd090e1eb21e75369d625
Jian Guan <61915096+tdq45gj@users.noreply.github.com>
2026-08-12 14:01:09 -0400

    Update release.go

--- release/release.go
+++ release/release.go
@@ -1261,6 +1261,7 @@ var linuxRepoVersionsStable = []LinuxRepo{
 	{"6.0", "6.0.0"}, // any 6.0 stable release version will send the package to the "6.0" repo
 	{"7.0", "7.0.0"}, // any 7.0 stable release version will send the package to the "7.0" repo
 	{"8.0", "8.0.0"}, // any 8.0 stable release version will send the package to the "8.0" repo
+	{"8.2", "8.2.0"}, // any 8.2 stable release version will send the package to the "8.2" repo
 	{"8.3", "8.3.0"}, // any 8.3 stable release version will send the package to the "8.3" repo
 	{"9.0", "9.0.0"}, // any 9.0 stable release version will send the package to the "9.0" repo
 }
