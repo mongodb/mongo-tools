ddbc5533d8609e060643db3a83080b6db821283d
Dave Rolsky <autarch@urth.org>
2026-08-31 14:42:01 -0500

    Simplify mise integration with node/npm
    If all NPM-installed tools are run via `mise exec`, we don't ever need to run `npm install` to
    create a `node_modules` dir. The tools just work when run from the tree under `$HOME/.local/share/mise`.

--- etc/github-codeowners-wrapper.sh
+++ etc/github-codeowners-wrapper.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 STATUS=0
 
-OUTPUT=$(./node_modules/.bin/github-codeowners audit --unloved --only-git)
+OUTPUT=$(mise exec npm:github-codeowners -- github-codeowners audit --unloved --only-git)
 
 if [ -n "$OUTPUT" ]; then
     echo "The repo contains unowned files. Please update the '.github/CODEOWNERS' file to include these files:"
@@ -15,7 +15,7 @@ if [ -n "$OUTPUT" ]; then
     STATUS=1
 fi
 
-OUTPUT=$(./node_modules/.bin/github-codeowners validate 2>&1)
+OUTPUT=$(mise exec npm:github-codeowners -- github-codeowners validate 2>&1)
 if [ -n "$OUTPUT" ]; then
     echo "The '.github/CODEOWNERS' file is not valid:"
     echo ""
